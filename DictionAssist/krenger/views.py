6b2fc782ce460c981802c255d578a1f8ebb67fb1
Sebastian Wu <68798342+sebswU@users.noreply.github.com>
2023-01-19 07:20:04 -0500

    functioning aws

--- DictionAssist/krenger/views.py
+++ DictionAssist/krenger/views.py
@@ -5,9 +5,10 @@ from krenger.models import Person,TxtRec, WordCard
 from krenger.forms import inputForm
 from django.urls import reverse
 import boto3
-import datetime
+import requests
 import time
 import os
+import json
 # Create your views here
 
 def view(request):
@@ -19,47 +20,80 @@ def view(request):
     #TODO: troubleshoot api for aws
         fileFormat = "mp3"
         location = "us-east-1"
-        s3URI = f"s3://webapp2012/audio_files/"
+        s3URI = "s3://webapp2012/"
         lang = "en-US"
         form = inputForm(request.POST, request.FILES)
         data = request.FILES['audio']
         text = request.POST['text']
         if form.is_valid():
-            open('DictionAssist/down/transcripts.txt', 'w').write(text)
-                
-            s3 = boto3.resource('s3')
-            bucket = s3.Bucket('webapp2012')
-            Key=f'{time.time()}'
-            bucket.put_object(Key=Key, Body=data)
+
+            open(os.path.join(os.path.dirname(__file__),"down/textRef.txt"), 'w').write(text)
+            print(data.chunks())
+            s3 = boto3.client('s3')
+            Key=f'{int(time.time())}'
+
+            s3.upload_fileobj(data,'webapp2012',f'{Key}.mp3')
+            
             client = boto3.client('transcribe',region_name=location)
             jobName = Key
             #transcribe and save to s3 bucket
             client.start_transcription_job(
                 TranscriptionJobName = jobName,
                 Media={
-                    'MediaFileUri': s3URI+Key+'/'
+                    'MediaFileUri': s3URI+Key+'.mp3'
                 },
                 MediaFormat=fileFormat,
                 LanguageCode = lang,
                 OutputBucketName = 'webapp2012',
                 OutputKey = 'transcription_results/'
             )
-            chances = 60
-            #get the location of the saved transcript file
-            while chances > 0:
-                chances -= 1
-                job = client.get_transcription_job(TranscriptionJobName=jobName)
-                jobLoc = job['Transcript']['TranscriptFileUri']
-                print(f"job {jobLoc}")
-                break
+            #transcription job will take about 20 seconds
+            time.sleep(20)
             #download transcript file
             s3 = boto3.resource('s3')
             bucket = s3.Bucket('webapp2012')
-            with open('DictionAssist/down/transcripts.txt','wb') as data:
-                bucket.download_file('down', data)
+            with open(os.path.join(os.path.dirname(__file__),"down/transcript.json"),'wb') as dt:
+                try:
+                    bucket.download_fileobj(f'transcription_results/{Key}.json', dt)
+                except:
+                    return HttpResponseNotFound("<h1>Try reloading the previous page and reentering form</h1>")
+
             #get the content of the file and then return it
-            content = open('../down/transcripts/txt','r').read()
-            print(content)
+            content = json.load(open(os.path.join(os.path.dirname(__file__),"down/transcript.json"),'rb'))
+            content=content['results']['transcripts'][0]['transcript']
+            stutterBool = False
+            diff = 0
+            #measure the difference in two strings
+            text1 = content
+            text2 = text
+
+            text1 = text1.split()
+            text2 = text2.split()
+            if len(text1)>len(text2):
+                diff = len(text1)-len(text2)
+            for i in range(len(text2)):
+                #differences in the length of text/transcript accounted for
+                if text1[i]!=text2[i]: 
+                    while diff!=0:
+                        if text1[i] == text2[i+1]:
+                            i+=1
+                            break
+                        else:
+                            diff-=1
+                            i+=1
+                    if diff==0:
+                        try:
+                            content = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{text2[i]}").content
+                            w = WordCard(word=text2[i],pronunc=content[0]['phonetics'][0]['audio'],textPronunc=content[0]['phonetic'],define=content[0]['meanings'][0]['definitions'][0]['definition'])
+                            w.save()
+                            
+                            print(f"you did not pronounce {text2[i]} correctly")
+                            i-=len(text1)-len(text2)
+                        except:
+                            raise ValueError
+            #take user to the page with the missed words
+            return HttpResponseRedirect(reverse('krenger:archive'))           
+            
             return HttpResponseRedirect(reverse('krenger:home'))
         else:
             form=inputForm();
@@ -85,7 +119,7 @@ class Settings(TemplateView):
     def __str__(self):
         return self.name
     
-class WordArchive(TemplateView):
+class WordArchive(DetailView):
     template_name = 'krenger/templates/words.html'
     def view(request, template_name):
         
@@ -94,7 +128,7 @@ class WordArchive(TemplateView):
         else:
             
             model = WordCard
-            return render(request,template_name)
+            return render(request,template_name,context={'db':model})
     
     
     def __str__(self):
